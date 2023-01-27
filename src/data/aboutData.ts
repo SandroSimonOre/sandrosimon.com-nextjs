1953c3da3d6e80ed8dda19c5d3028f594c150d0b
SANDRO SIMON <sandrosimonore@gmail.com>
2023-01-27 10:42:40 -0500

    Abou data updated...

--- src/data/aboutData.ts
+++ src/data/aboutData.ts
@@ -1,15 +1,62 @@
 export const aboutData = [
-    {show: true, title: "Who I am?", paragraphs: ["An accounting graduate"]},
-    {show: true, title: "Which technologies I work with?", paragraphs: ["Javascript, React.js, Next.js, Docker, Git, Python, Django, Kubernetes"]},
-    {show: true, title: "Why am I leaving accounting?", paragraphs: ["I love technology!"]},
-    {show: true, title: "What about my experience?", paragraphs: [
-        "When I was working as an accountan I built a program",
-        "It was built in Excel VBA and it connected to a MySQL Database."
-    ]},
-    {show: true, title: "Which technologies am I planning to study?", paragraphs: ["Golang, Kotlin, Data Analyst, FastAPI"]},
-    {show: true, title: "What about my English?", paragraphs: ["I'm making some videos. I thing is a god fashion to learn. besides that I read books in english, so in that way I learn english and technoligy at the same time"]},
-    {show: true, title: "How do I study?", paragraphs: ["Udemy, Youtube, Platzi. That was  at first. Now I learn by making projects"]},
-    {show: true, title: "Why should you hire me?", paragraphs: ["I am an applied student, always willing to learn new technologies."]},
+    {
+        show: true,
+        title: "Who I am?",
+        paragraphs: [
+            "An accounting graduate. I also have a master degree in Taxes. I love taxes and Financial, they are part of my life. However, I have always loved to wotk with technology. In special, I love to build programs. My first program was made in a calculator. It helped me to control my expenses. Then, I learned programming in VBA for applications ans SQL and what knoledge y solved problems in my work as accountant. "
+        ]
+    },
+    {
+        show: true,
+        title: "Which technologies I work with?",
+        paragraphs: [
+            "Javascript, React.js, Next.js, Docker, Git, Python, Django, Kubernetes",
+            "I am also devoting some time (around 2 hours a day) to learning new languages: Python, Golang and Python. Cloud technologies are important nowadays, so I have take some courses in AWS."
+        ]
+    },
+    {
+        show: true, 
+        title: "Why am I leaving accounting?", 
+        paragraphs: [
+            "I love technology!. Accounting, and especially Taxes and Finance. They are part of mi life. However, programmig makes me happy. So, I want to work on it."
+        ]
+    },
+    {
+        show: true, 
+        title: "What about my experience?", 
+        paragraphs: [
+            "When I was working as an accountan I built a program",
+            "It was built in Excel VBA and it connected to a MySQL Database."
+        ]
+    },
+    {
+        show: true, 
+        title: "Which technologies am I planning to study?", 
+        paragraphs: [
+            "Golang, Kotlin, Data Analyst, FastAPI"
+        ]
+    },
+    {
+        show: true, 
+        title: "What about my English?", 
+        paragraphs: [
+            "I'm making some videos. I thing is a god fashion to learn. besides that I read books in english, so in that way I learn english and technoligy at the same time"
+        ]
+    },
+    {
+        show: true, 
+        title: "How do I study?", 
+        paragraphs: [
+            "Udemy, Youtube, Platzi. That was  at first. Now I learn by making projects"
+        ]
+    },
+    {
+        show: true, 
+        title: "Why should you hire me?", 
+        paragraphs: [
+            "I am an applied student, always willing to learn new technologies."
+        ]
+    },
     {
         show: true,
         title: "You are an accountant, why did you decide to start in programming?", 
